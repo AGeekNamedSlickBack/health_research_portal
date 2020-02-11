8baf943a23f96f772c53a638434043ad88da99d1
kentay <mathenge362@gmail.com>
2020-02-11 13:55:22 +0300

    Categorize the scraped researches

--- tests/researches/test_model.py
+++ tests/researches/test_model.py
@@ -14,3 +14,14 @@ def test_research_model(research):
 def test_research_str_representation(research):
     """Test title string rep."""
     assert str(research) == "Kitu flani researched"
+
+
+def test_research_category(research_category, research):
+    """Test research category."""
+    assert research_category.research == research
+    assert research_category.category == "malaria"
+
+
+def test_research_category_str_representation(research_category):
+    """Test title string rep."""
+    assert str(research_category) == "malaria"
